dca7733bb9a625753e0415e16e2918bb7805b425
Calin Lupas <lupas_calin@yahoo.com>
2021-12-08 21:37:31 -0500

    Added the disclaimer notice to all Program files.

--- NetCoreAsyncProgramming/AP.AsyncCookingBreakfast/Program.cs
+++ NetCoreAsyncProgramming/AP.AsyncCookingBreakfast/Program.cs
@@ -1,6 +1,11 @@
-﻿using AP.AsyncCookingBreakfast.Models;
+﻿/*
+ * Notice: This Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment.
+ * THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+using AP.AsyncCookingBreakfast.Models;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 /// <summary>
--- NetCoreAsyncProgramming/AP.HttpRequestsHttpClientFactoryConsole/Program.cs
+++ NetCoreAsyncProgramming/AP.HttpRequestsHttpClientFactoryConsole/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * Notice: This Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment.
+ * THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
